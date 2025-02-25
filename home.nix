f1a19eeddd98f773acf11fab74acbaf1f2ebf809
Excigma <git@excigma.xyz>
2025-02-25 13:59:44 +1300

    fix: enable zsh config

--- home.nix
+++ home.nix
@@ -30,6 +30,7 @@
   };
 
   programs.zsh = {
+    enable = true;
     initExtraFirst = ''
       # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
       # Initialization code that may require console input (password prompts, [y/n]
