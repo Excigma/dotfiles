fecafb9fe0a19caee1d8fee27b3a655a4f7f8a81
Excigma <git@excigma.xyz>
2025-02-25 14:01:56 +1300

    chore: disable autocd

--- home.nix
+++ home.nix
@@ -42,7 +42,6 @@
       # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
       [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
     '';
-    autocd = true;
     shellAliases = {
       ".." = "cd ..";
       "grep" = "grep --color=auto";
