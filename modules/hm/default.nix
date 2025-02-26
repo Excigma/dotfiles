6c5e3e889afa78b9b20ba9724493970d2380cb76
Excigma <git@excigma.xyz>
2025-02-26 20:37:07 +1300

    feat: command not found handler from @ShaneTRS

--- modules/hm/default.nix
+++ modules/hm/default.nix
@@ -30,6 +30,7 @@
   };
 
   programs = {
+    nix-index.enable = true;
     ghostty = {
       enable = true;
       enableZshIntegration = true;
@@ -150,6 +151,46 @@
 
         zstyle ':autocomplete:*' min-input 3
         zstyle ':autocomplete:*' delay 0.1
+
+        nix-run() {
+          NIXPKGS_ALLOW_UNFREE=1 nix shell --impure "nixpkgs#$1" \
+            --command sh -c "which ''${1#*.} &>/dev/null && exec ''${1#*.} ''${*:2}; exec ''${*:2}"
+        }
+        nix-shell() {(
+          for i in "$@"; do
+            if [ -n "$OPTION" ] || [[ "''${i:0:1}" == "-" ]]; then
+              ARGS+=" \"$i\""
+              OPTION=1; continue
+            fi
+            NIX_SHELL_PACKAGES+=" $i";
+            ARGS+=" \"nixpkgs#$i\""
+          done
+          eval "NIX_SHELL_PACKAGES=\"''${NIX_SHELL_PACKAGES#* }\" NIXPKGS_ALLOW_UNFREE=1 nix shell --impure $ARGS"
+        )}
+        where() { readlink -f "$(which "$@")"; }
+
+        SGR () { for i in "$@"; do echo -ne "\e[$i"m; done; }
+        nix-find() { nix-locate --no-group --top-level -r "$@"; }
+        command_not_found_handler() {(
+          CMD="$1"; IFS=$'\n'
+          if [ "$NIX_MISSING" = "never" ]; then
+            echo "$(SGR 1 34)❭❭ $(SGR 0 1)$CMD$(SGR 0) not found! You can use $(SGR 1)nix-find -wtx /$CMD$(SGR 0) to find it" >&2
+            exit 127
+          fi
+          PACKAGES=($(nix-locate --minimal --no-group --type x --type s --top-level --whole-name --at-root "/bin/$CMD"))
+          case "''${#PACKAGES}" in
+            0) echo "$(SGR 1 34)❭❭ $(SGR 0 1)$CMD$(SGR 0) not found! Are you sure you've typed the command correctly?" >&2 ;;
+            1) [ "$NIX_MISSING" = "auto" ] &&
+                exec nix-shell "''${PACKAGES[1]}" --command "$@";
+              echo -n "$(SGR 1 34)❭❭ $(SGR 0 1)$CMD$(SGR 0) not found! Would you like to bring $(SGR 1)''${PACKAGES[1]%.*}$(SGR 0) into scope? " >&2; read
+              exec nix-shell "''${PACKAGES[1]}" --command "$@" ;;
+            *) [ "$NIX_MISSING" = "always" ] &&
+                exec nix-shell "''${PACKAGES[1]}" --command "$@";
+              echo "$(SGR 1 34)❭❭ $(SGR 0 1)$CMD$(SGR 0) not found! Would you like to bring one of the following packages into scope?" >&2
+              PS3=""; select PKG in ''${PACKAGES[@]%.*}; do exec nix-shell "$PKG" --command "$@"; done ;;
+          esac
+          exit 127
+        )}
       '';
       sessionVariables = {
         VISUAL = "code --wait";
