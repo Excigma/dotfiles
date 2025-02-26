c9df5db52d9c923ab7be47fc9a5feb49b2450d16
Excigma <git@excigma.xyz>
2025-02-26 17:11:11 +1300

    chore: move ghostty theme into home-manager

--- modules/hm/default.nix
+++ modules/hm/default.nix
@@ -33,18 +33,82 @@
     ghostty = {
       enable = true;
       enableZshIntegration = true;
-      settings = { theme = "dark:dark-theme,light:light-theme"; };
+      settings = {
+        theme = "dark:dark-theme,light:light-theme";
+        term = "xterm-256color";
+        keybind = ''
+          ctrl+t=new_tab
+          ctrl+w=close_tab
+        '';
+      };
+      themes = {
+        light-theme = {
+          palette = [
+            "0=#000000"
+            "1=#AA3731"
+            "2=#448C27"
+            "3=#CB9000"
+            "4=#325CC0"
+            "5=#7A3E9D"
+            "6=#0083B2"
+            "7=#BBBBBB"
+            "8=#777777"
+            "9=#F05050"
+            "10=#60CB00"
+            "11=#FFBC5D"
+            "12=#007ACC"
+            "13=#E64CE6"
+            "14=#00AACB"
+            "15=#FFFFFF"
+          ];
+          background = "#F7F7F7";
+          foreground = "#434343";
+          cursor-color = "#434343";
+          cursor-text = "#F7F7F7";
+          selection-background = "#BBBBBB";
+          selection-foreground = "#434343";
+        };
+        dark-theme = {
+          palette = [
+            "0=#0E1415"
+            "1=#e25d56"
+            "2=#73ca50"
+            "3=#e9bf57"
+            "4=#4a88e4"
+            "5=#915caf"
+            "6=#23acdd"
+            "7=#f0f0f0"
+            "8=#777777"
+            "9=#f36868"
+            "10=#88db3f"
+            "11=#f0bf7a"
+            "12=#6f8fdb"
+            "13=#e987e9"
+            "14=#4ac9e2"
+            "15=#FFFFFF"
+          ];
+          background = "#1e1e1e";
+          foreground = "#CECECE";
+          cursor-color = "#CECECE";
+          cursor-text = "#0E1415";
+          selection-background = "#f0f0f0";
+          selection-foreground = "#1e1e1e";
+        };
+      };
     };
+
     git = {
       enable = true;
       userName = "Excigma";
       userEmail = "git@excigma.xyz";
     };
+
     zoxide = {
       enable = true;
       enableZshIntegration = true;
       options = [ "--cmd cd" ];
     };
+
     zsh = {
       enable = true;
       defaultKeymap = "emacs";
