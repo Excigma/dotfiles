a6ea2563f8d1c83138057ec53bda033e898c3a0a
Excigma <git@excigma.xyz>
2025-02-26 20:37:56 +1300

    feat: iriun webcam attempt (doesn't work)

--- modules/nixos/hardware.nix
+++ modules/nixos/hardware.nix
@@ -7,8 +7,8 @@
   boot = {
     initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usbhid" "usb_storage" "sd_mod" ];
     initrd.kernelModules = [ ];
-    kernelModules = [ "kvm-intel" ];
-    extraModulePackages = [ ];
+    kernelModules = [ "kvm-intel" "v4l2loopback" ];
+    extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
   };
 
   fileSystems = {
