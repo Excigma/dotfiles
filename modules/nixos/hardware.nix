3fcfaebb77acc664329408215a43bf5baca3b095
Excigma <git@excigma.xyz>
2025-02-28 16:47:57 +1300

    feat: use xe experimental driver (what could go wrong)

--- modules/nixos/hardware.nix
+++ modules/nixos/hardware.nix
@@ -6,10 +6,10 @@
 
   boot = {
     initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usbhid" "usb_storage" "sd_mod" ];
-    initrd.kernelModules = [ ];
+    initrd.kernelModules = [ "xe" ];
     kernelPackages = pkgs.unstable.linuxPackages;
     kernelModules = [ "kvm-intel" "v4l2loopback" "snd-aloop" ];
-    kernelParams = [ "i915.fastboot=1" ];
+    kernelParams = [ "i915.fastboot=1" "i915.force_probe=!a7a1" "xe.force_probe=a7a1" ];
     extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
     extraModprobeConfig = ''
       options v4l2loopback exclusive_caps=1 devices=1 card_label="Iriun Webcam,Iriun Webcam #2,Iriun Webcam #3,Iriun Webcam #4"
