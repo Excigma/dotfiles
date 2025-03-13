5574fd8039a25123edd1fb901d3ae79065aa02e8
Excigma <git@excigma.xyz>
2025-03-13 16:47:48 +1300

    feat: use nix-index-database and update packages

--- flake.nix
+++ flake.nix
@@ -4,6 +4,11 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -14,7 +19,7 @@
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, ... }:
+  outputs = inputs@{ self, nixpkgs, nix-index-database, home-manager, ... }:
     let
       user = "excigma";
       config.allowUnfree = true;
@@ -42,7 +47,9 @@
           inherit system specialArgs;
           modules = [
             ./modules/nixos
+
             home-manager.nixosModules.home-manager
+            nix-index-database.nixosModules.nix-index
 
             (if builtins.pathExists ./secrets/default.nix then
               ./secrets
