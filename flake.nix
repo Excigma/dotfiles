75cb4baf186526df2e17ac7a9f2a8173df23bd88
Excigma <git@excigma.xyz>
2025-03-25 14:54:23 +1300

    chore: fix channels

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     nix-index-database = {
       url = "github:nix-community/nix-index-database";
--- modules/nixos/overlays.nix
+++ modules/nixos/overlays.nix
@@ -4,7 +4,7 @@
 
     (final: prev: {
       unstable = import nixpkgs-unstable { inherit (prev) config system; };
-      stable = import nixpkgs-unstable { inherit (prev) config system; };
+      stable = import nixpkgs-stable { inherit (prev) config system; };
 
       sound-theme-freedesktop = prev.sound-theme-freedesktop.overrideAttrs (oldAttrs: {
         postInstall = (oldAttrs.postInstall or "") + "rm -f $out/share/sounds/freedesktop/stereo/screen-capture.oga";
