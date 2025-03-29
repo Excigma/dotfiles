f707f5f94f5cc2df269e31cf17546c46ac64cf22
Excigma <git@excigma.xyz>
2025-03-30 00:38:41 +1300

    chore: update packages and fix marble patches

--- modules/nixos/overlays.nix
+++ modules/nixos/overlays.nix
@@ -10,61 +10,44 @@
         postInstall = (oldAttrs.postInstall or "") + "rm -f $out/share/sounds/freedesktop/stereo/screen-capture.oga";
       });
 
-      marble-shell-theme = let
-        no-backgrounds = ''
-          diff --git a/theme/gnome-shell/.css/panel.css b/theme/gnome-shell/.css/panel.css
-          index 43b478b..6c67062 100644
-          --- a/theme/gnome-shell/.css/panel.css
-          +++ b/theme/gnome-shell/.css/panel.css
-          @@ -36,6 +36,13 @@
-               box-shadow: inset 0 0 0 1px BORDER-SHADOW;
-           }
+      marble-shell-theme = prev.marble-shell-theme.overrideAttrs (oldAttrs: {
+        patches = [
+          (builtins.toFile "single-path" ''
+            diff --git a/theme/gnome-shell/.css/panel.css b/theme/gnome-shell/.css/panel.css
+            index 43b478b..6c67062 100644
+            --- a/theme/gnome-shell/.css/panel.css
+            +++ b/theme/gnome-shell/.css/panel.css
+            @@ -36,6 +36,13 @@
+                 box-shadow: inset 0 0 0 1px BORDER-SHADOW;
+             }
 
-          +/* App icons taskbar - hide button background */
-          +.panel-button.azTaskbar-BaseIcon {
-          +    background-color: transparent !important;
-          +    border: none !important;
-          +    box-shadow: none !important;
-          +}
-          +
-           .panel-button:hover,
-           .panel-button:hover .clock,
-           .panel-button:active,
-        '';
-        fix-padding = ''
-          diff --git a/theme/gnome-shell/.css/quick-settings.css b/theme/gnome-shell/.css/quick-settings.css
-          index fcf8f86..e5f5c42 100644
-          --- a/theme/gnome-shell/.css/quick-settings.css
-          +++ b/theme/gnome-shell/.css/quick-settings.css
-          @@ -2,7 +2,7 @@
-           
-           /* QS section */
-           .quick-settings {
-          -	padding: 15px;
-          +	padding: 15px !important;
-           	border-radius: 24px;
-           }
-        '';
-      in prev.marble-shell-theme.overrideAttrs (oldAttrs: {
-        version = "47.0";
-        nativeBuildInputs = with pkgs; [ python3 dconf gnome-shell ];
-        src = pkgs.fetchFromGitHub {
-          owner = "imarkoff";
-          repo = "Marble-shell-theme";
-          rev = "1e83e073f7e50eaaf82763edbdae59585ca5e585";
-          hash = "sha256-+uPjwOUwrdFfBvpWtuZhe789v2xvZG3XeFyYw8HP8QM=";
-        };
-        patchPhase = ''
-          runHook prePatch
-          substituteInPlace scripts/config.py --replace-fail "~/.themes" ".themes"
-          patch -p1 << EOF
-          ${no-backgrounds}
-          EOF
-          patch -p1 << EOF
-          ${fix-padding}
-          EOF
-          runHook postPatch
-        '';
+            +/* App icons taskbar - hide button background */
+            +.panel-button.azTaskbar-BaseIcon {
+            +    background-color: transparent !important;
+            +    border: none !important;
+            +    box-shadow: none !important;
+            +}
+            +
+             .panel-button:hover,
+             .panel-button:hover .clock,
+             .panel-button:active,
+          '')
+
+          (builtins.toFile "single-path" ''
+            diff --git a/theme/gnome-shell/.css/quick-settings.css b/theme/gnome-shell/.css/quick-settings.css
+            index fcf8f86..e5f5c42 100644
+            --- a/theme/gnome-shell/.css/quick-settings.css
+            +++ b/theme/gnome-shell/.css/quick-settings.css
+            @@ -2,7 +2,7 @@
+             
+             /* QS section */
+             .quick-settings {
+            -	padding: 15px;
+            +	padding: 15px !important;
+             	border-radius: 24px;
+             }
+          '')
+        ];
       });
 
       go-10mb-video = pkgs.buildGoModule {
