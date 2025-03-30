4668be8ffaa59b33112431157490fe7c253f350a
Excigma <git@excigma.xyz>
2025-03-30 23:23:06 +1300

    chore: patch ppd to use different platform_profile

--- modules/nixos/overlays.nix
+++ modules/nixos/overlays.nix
@@ -134,6 +134,30 @@
           '')
         ] ++ old.patches or [ ];
       });
+
+      power-profiles-daemon = prev.power-profiles-daemon.overrideAttrs (old: {
+        # Disable tests, and they take a long time to run.
+        mesonFlags = (old.mesonFlags or [ ]) ++ [ "-Dtests=false" ];
+        patches = [
+          (builtins.toFile "single-path" ''
+            diff --git a/src/ppd-driver-platform-profile.c b/src/ppd-driver-platform-profile.c
+            index 28fc335..706ab82 100644
+            --- a/src/ppd-driver-platform-profile.c
+            +++ b/src/ppd-driver-platform-profile.c
+            @@ -66,7 +66,7 @@ profile_to_acpi_platform_profile_value (PpdDriverPlatformProfile *self,
+                   return "low-power";
+                 return "quiet";
+               case PPD_PROFILE_BALANCED:
+            -    return "balanced";
+            +    return "cool";
+               case PPD_PROFILE_PERFORMANCE:
+                 return "performance";
+               }
+            -- 
+            2.48.1
+          '')
+        ] ++ old.patches or [ ];
+      });
     })
   ];
 }
