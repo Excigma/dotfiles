a2add3ee3e0a13331c6fd6c191890e7eed8913d7
Excigma <git@excigma.xyz>
2025-04-02 22:41:59 +1300

    chore: reenable hw volume

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -94,7 +94,7 @@ in {
             ];
 
             "bluez5.hfphsp-backend" = "none";
-            "bluez5.enable-hw-volume" = "false";
+            # "bluez5.enable-hw-volume" = "false";
           };
         };
       };
