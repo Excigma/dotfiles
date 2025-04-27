262f61e54c2220070053a8306ecf6253181b2572
Excigma <git@excigma.xyz>
2025-04-27 13:48:22 +1200

    chore: switch to solaar for logitech mappings

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -66,19 +66,6 @@ in
       powerKey = "suspend";
       powerKeyLongPress = "poweroff";
     };
-    keyd = {
-      enable = true; # Turn on keyd daemon
-      keyboards.default = {
-        # Logitech M510
-        ids = [ "046d:4051" ];
-        settings = {
-          main = {
-            mouse1 = "C-M-left"; # Side button to Ctrl+Meta+Left
-            mouse2 = "C-M-right"; # Side button to Ctrl+Meta+Right
-          };
-        };
-      };
-    };
     pipewire = {
       enable = true;
       alsa.enable = true;
@@ -225,6 +212,7 @@ in
 
   hardware = {
     sensor.iio.enable = true;
+    logitech.wireless.enable = true;
     graphics = {
       enable = true;
       extraPackages = with pkgs; [
@@ -476,6 +464,7 @@ in
 
           prismlauncher
           nautilus-python
+          solaar
 
           zotero
         ]
