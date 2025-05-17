64f40e036401b87dd688c24e018802725ad65f3c
Excigma <git@excigma.xyz>
2025-05-17 22:55:19 +1200

    chore: remove unneeded cloudflared patch

--- modules/overlays/patches.nix
+++ modules/overlays/patches.nix
@@ -2,32 +2,6 @@
 {
   nixpkgs.overlays = [
     (final: prev: {
-      cloudflared =
-        (prev.cloudflared.override {
-          buildGoModule = pkgs.buildGoModule.override {
-            go = pkgs.buildPackages.go_1_23.overrideAttrs (prevAttrs: {
-              pname = "cloudflare-go";
-              version = "1.22.5-devel-cf";
-
-              src = pkgs.fetchFromGitHub {
-                owner = "cloudflare";
-                repo = "go";
-                rev = "af19da5605ca11f85776ef7af3384a02a315a52b";
-                hash = "sha256-6VT9CxlHkja+mdO1DeFoOTq7gjb3T5jcf2uf9TB/CkU=";
-              };
-
-              patches = map (
-                patch:
-                if (baseNameOf patch == "go_no_vendor_checks-1.23.patch") then
-                  ./patches/cloudflare-go-no-vendor-1.22.patch
-                else
-                  patch
-              ) prevAttrs.patches;
-            });
-          };
-        }).overrideAttrs
-          { meta.broken = false; };
-
       marble-shell-theme = prev.marble-shell-theme.overrideAttrs (prevAttrs: {
         patches = [
           ./patches/marble-disable-taskbar-icon-background.patch
