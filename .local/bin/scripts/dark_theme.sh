db9606d3c4262578186943081e989d73fcede984
Excigma <git@excigma.xyz>
2025-05-17 23:27:40 +1200

    fix: broken theme toggle command

--- .local/bin/scripts/dark_theme.sh
+++ .local/bin/scripts/dark_theme.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-gsettings set org.gnome.shell.extensions.user-theme name 'Marble-blue-dark'
-gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3-dark
+dconf write /org/gnome/shell/extensions/user-theme/name "'Marble-blue-dark'"
+dconf write /org/gnome/desktop/interface/gtk-theme "'adw-gtk3-dark'"
 # sed -i 's/style=Adwaita/style=Adwaita-Dark/g' ~/.config/qt5ct/qt5ct.conf &
--- .local/bin/scripts/light_theme.sh
+++ .local/bin/scripts/light_theme.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-gsettings set org.gnome.shell.extensions.user-theme name 'Marble-blue-light'
-gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3
+dconf write /org/gnome/shell/extensions/user-theme/name "'Marble-blue-light'"
+dconf write /org/gnome/desktop/interface/gtk-theme "'adw-gtk3'"
 # sed -i 's/style=Adwaita-Dark/style=Adwaita/g' ~/.config/qt5ct/qt5ct.conf &
