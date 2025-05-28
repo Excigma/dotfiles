99ff5355963607947fca6e7a703355ff9c73a622
Excigma <git@excigma.xyz>
2025-05-28 22:36:42 +1200

    chore: update to 25.05 and switch to stable

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   description = "NixOS configuration";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     nix-index-database = {
