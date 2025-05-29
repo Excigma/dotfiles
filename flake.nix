e6bef9ef335eb647b6862ab0e5db6aff765f5d2d
Excigma <git@excigma.xyz>
2025-05-29 16:40:22 +1200

    chore: switch home manager to 25.05

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nur = {
