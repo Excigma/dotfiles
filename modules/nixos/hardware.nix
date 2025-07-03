5991102515a2944956e15a01a553e2e67314eb76
Excigma <git@excigma.xyz>
2025-07-03 16:53:31 +1200

    fix: switch back to intel i915 drivers

--- modules/nixos/hardware.nix
+++ modules/nixos/hardware.nix
@@ -29,8 +29,6 @@
     ];
     kernelParams = [
       "i915.fastboot=1"
-      "i915.force_probe=!a7a1"
-      "xe.force_probe=a7a1"
     ];
     extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
     extraModprobeConfig = ''
