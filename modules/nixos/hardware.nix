944831d2d8c7f63be42f5841dfe90100a3d4ca49
Excigma <git@excigma.xyz>
2025-07-29 19:18:15 +1200

    chore: remove unneeded flags for v4l2loopback

--- modules/nixos/hardware.nix
+++ modules/nixos/hardware.nix
@@ -33,7 +33,7 @@
     ];
     extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
     extraModprobeConfig = ''
-      options v4l2loopback exclusive_caps=1 devices=1 card_label="Iriun Webcam,Iriun Webcam #2,Iriun Webcam #3,Iriun Webcam #4"
+      options v4l2loopback exclusive_caps=1 devices=1 card_label="Iriun Webcam"
       options snd-aloop index=0
     '';
   };
