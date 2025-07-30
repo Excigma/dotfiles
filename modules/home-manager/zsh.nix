e2b785b7ab7a08d7529b3fb0b6c7c717b4acb44a
Excigma <git@excigma.xyz>
2025-07-30 12:41:19 +1200

    chore: remove command not found handler

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -74,46 +74,6 @@ in
           fi
           command sudo "$@"
         }
-
-        nix-run() {
-          NIXPKGS_ALLOW_UNFREE=1 nix shell --impure "nixpkgs#$1" \
-            --command sh -c "which ''${1#*.} &>/dev/null && exec ''${1#*.} ''${*:2}; exec ''${*:2}"
-        }
-        nix-shell() {(
-          ARGS=()
-          for i in "$@"; do
-            if [[ -n $OPTION || $i[1] = - ]]; then
-              ARGS+="$i" OPTION=1
-               continue
-            fi
-            ARGS+="nixpkgs#$i"
-          done
-          IN_NIX_SHELL=impure NIXPKGS_ALLOW_UNFREE=1 nix shell --impure "''${ARGS[@]}"
-        )}
-        where() { readlink -f "$(which "$@")"; }
-
-        nix-find() { ${pkgs.nix-index}/bin/nix-locate --no-group --top-level -r "$@"; }
-        command_not_found_handler() {(
-          CMD="$1" IFS=$'\n'
-          SGR() { echo -ne "\e[''${(j:m\e[:)@}m"; }
-          if [ "$NIX_MISSING" = "never" ]; then
-            echo "$(SGR 1 34)❭❭ $(SGR 0 1)$CMD$(SGR 0) not found! You can use $(SGR 1)nix-find -wtx /$CMD$(SGR 0) to find it" >&2
-            exit 127
-          fi
-          PACKAGES=($(${pkgs.nix-index}/bin/nix-locate --minimal --no-group --type x --type s --top-level --whole-name --at-root "/bin/$CMD"))
-          case "''${#PACKAGES}" in
-            0) echo "$(SGR 1 34)❭❭ $(SGR 0 1)$CMD$(SGR 0) not found! Are you sure you've typed the command correctly?" >&2 ;;
-            1) [ "$NIX_MISSING" = "auto" ] &&
-                exec nix-shell "''${PACKAGES[1]}" --command "$@";
-              echo -n "$(SGR 1 34)❭❭ $(SGR 0 1)$CMD$(SGR 0) not found! Would you like to bring $(SGR 1)''${PACKAGES[1]%.*}$(SGR 0) into scope? " >&2; read
-              exec nix-shell "''${PACKAGES[1]}" --command "$@" ;;
-            *) [ "$NIX_MISSING" = "always" ] &&
-                exec nix-shell "''${PACKAGES[1]}" --command "$@";
-              echo "$(SGR 1 34)❭❭ $(SGR 0 1)$CMD$(SGR 0) not found! Would you like to bring one of the following packages into scope?" >&2
-              PS3=""; select PKG in ''${PACKAGES[@]%.*}; do exec nix-shell "$PKG" --command "$@"; done ;;
-          esac
-          exit 127
-        )}
       '';
       sessionVariables = {
         VISUAL = "${getExe pkgs.vscode} --wait";
