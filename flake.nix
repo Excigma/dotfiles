634d8842eaf54a1e5d53d8d0e22d77258b64b1b9
Excigma <git@excigma.xyz>
2025-12-21 20:56:45 +1300

    chore: adjust auto gc to 7 days
    as I don't have much disk space

--- flake.nix
+++ flake.nix
@@ -72,7 +72,7 @@
               nix = {
                 gc = {
                   automatic = true;
-                  options = "--delete-older-than 30d";
+                  options = "--delete-older-than 7d";
                 };
                 optimise.automatic = true;
                 registry.pkgs.flake = self;
